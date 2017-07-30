1f845836f0dee896f0de07b5b13efd2d280663e0
Mark Hansen <mark@markhansen.co.nz>
2017-07-30 17:52:53 +1000

    Add support for heaps of directives

--- tests/test.ts
+++ tests/test.ts
@@ -2,21 +2,14 @@ import * as assert from 'assert';
 import * as fs from 'fs';
 import build from './build';
 
-let files = [
-  "Accounts",
-  "AccountsLowercase",
-  "CommentsSemicolon",
-  "CommentsOctothorpe",
-  "Currencies",
-  "Dates",
-  "Include",
-];
+let files = fs.readdirSync('tests/cases/', {encoding:'utf8'}).filter(f => f.match(/.in.hledger$/));
 
 describe('Hledger test case files. build($name.in.hledger)=$name.want', () => {
   for (let f of files) {
     it(f, () => {
-      let inHledger = fs.readFileSync(`tests/cases/${f}.in.hledger`, {encoding: 'utf8'});
-      let want = fs.readFileSync(`tests/cases/${f}.want`, {encoding: 'utf8'});
+      let inHledger = fs.readFileSync(`tests/cases/${f}`, {encoding: 'utf8'});
+      let wantFilename = 'tests/cases/' + f.substring(0, f.length - '.in.hledger'.length) + '.want';
+      let want = fs.readFileSync(wantFilename, {encoding: 'utf8'});
       let got = build(inHledger);
       assert.equal(got, want);
     });
