3245edf9ea36f36d6573c6f35dbb0fa3686c235e
Mark Hansen <mark@markhansen.co.nz>
2017-07-30 17:21:47 +1000

    Make it possible to convert an entire directory at once
    $ ts-node tests/main.ts tests/cases/**.in.hledger

--- tests/main.ts
+++ tests/main.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import build from './build';
 import * as parseArgs from 'minimist';
 
-for (let arg of parseArgs(process.argv.slice(2))._) {
-  console.log(build(fs.readFileSync(arg, {encoding:'utf8'})));
+for (let inFile of parseArgs(process.argv.slice(2))._) {
+  let outFile = inFile.substring(0, inFile.length - ".in.hledger".length) + ".want";
+  let converted = build(fs.readFileSync(inFile, {encoding:'utf8'}));
+  fs.writeFileSync(outFile, converted, {encoding: 'utf8'});
 }
