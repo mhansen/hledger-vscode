757b32a430fa121081f8920823e4d8082bd00975
Mark Hansen <mark@markhansen.co.nz>
2021-02-13 13:08:29 +1100

    Remove top-level async.
    It's unsupported now, for some reason, despite working before.
    
    https://github.com/TypeStrong/ts-node/issues/935

--- tests/main.ts
+++ tests/main.ts
@@ -2,8 +2,12 @@ import * as fs from 'fs';
 import build from './build';
 import * as parseArgs from 'minimist';
 
-for (let inFile of parseArgs(process.argv.slice(2))._) {
-  let outFile = inFile.substring(0, inFile.length - ".in.hledger".length) + ".want";
-  let converted : string = await build(fs.readFileSync(inFile, {encoding:'utf8'}));
-  fs.writeFileSync(outFile, converted, {encoding: 'utf8'});
+async function main() {
+  for (let inFile of parseArgs(process.argv.slice(2))._) {
+    let outFile = inFile.substring(0, inFile.length - ".in.hledger".length) + ".want";
+    let converted: string = await build(fs.readFileSync(inFile, { encoding: 'utf8' }));
+    fs.writeFileSync(outFile, converted, { encoding: 'utf8' });
+  }
 }
+
+main();
