85498a56d67a878246c29776ee7c52b765d03501
Mark Hansen <mark@markhansen.co.nz>
2021-06-19 20:41:15 +1000

    Switch to new oniguruma bindings
    The old ones hadn't been updated in 2 years
    
    Microsoft's VS Code bindings are up to date
    
    Used example code from https://github.com/microsoft/vscode-textmate to migrate.

--- tests/build.ts
+++ tests/build.ts
@@ -1,24 +1,41 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as vsctm from 'vscode-textmate/release/main';
-import * as oniguruma from 'oniguruma';
+import * as oniguruma from 'vscode-oniguruma';
+
+/**
+ * Utility to read a file as a promise
+ */
+function readFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, data) => error ? reject(error) : resolve(data));
+  })
+}
+
+const wasmBin = fs.readFileSync(path.join(__dirname, '../node_modules/vscode-oniguruma/release/onig.wasm')).buffer;
+const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
+  return {
+    createOnigScanner(patterns) { return new oniguruma.OnigScanner(patterns); },
+    createOnigString(s) { return new oniguruma.OnigString(s); }
+  };
+});
+
+// Create a registry that can create a grammar from a scope name.
+const registry = new vsctm.Registry({
+  onigLib: vscodeOnigurumaLib,
+  loadGrammar: async (scopeName) => {
+    const fileName = 'syntaxes/hledger.tmLanguage.json';
+    if (scopeName === 'source.hledger') {
+      // https://github.com/textmate/javascript.tmbundle/blob/master/Syntaxes/JavaScript.plist
+      const data = await readFile(fileName);
+      return vsctm.parseRawGrammar(data.toString(), fileName);
+    }
+    console.log(`Unknown scope name: ${scopeName}`);
+    return null;
+  }
+});
 
 export default async function build(input: string) : Promise<string> {
-  const registry = new vsctm.Registry({
-    onigLib: Promise.resolve({
-      createOnigScanner: (sources) => new oniguruma.OnigScanner(sources),
-      createOnigString: (str) => new oniguruma.OnigString(str)
-    }),
-    loadGrammar: async (scopeName: string) => {
-      // https://github.com/textmate/javascript.tmbundle/blob/master/Syntaxes/JavaScript.plist
-      const fileName = 'syntaxes/hledger.tmLanguage.json';
-      if (scopeName === 'source.hledger') {
-        let g = vsctm.parseRawGrammar(fs.readFileSync(fileName).toString(), fileName)
-        return g;
-      }
-      console.log(`Unknown scope name: ${scopeName}`);
-      return null;
-    }
-  });
   const grammar = await registry.loadGrammar('source.hledger');
 
   let ruleStack = null;
